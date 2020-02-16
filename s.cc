155b41c3bfeefb0774d716397e320b302214f22c
Jeffery Parker <36011518+peffjarker@users.noreply.github.com>
2020-02-16 13:00:18 -0500

    Add files via upload

---
+++ s.cc
@@ -0,0 +1,98 @@
+/*
+Jeff Parker
+2/15/2020
+HW2
+This program is a parallel version for a program that detects
+the amount of pixels turned on in a given region on a black
+and white image
+*/
+#include <iomanip>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Count_Pixels {
+public:
+  /*
+  checks to see if given i and j is in the range of the image
+  */
+  bool valid(int i, int j, vector<vector<int>> &image) {
+    if (i >= 0 && i < image.size()) {
+      if (j >= 0 && j < image[0].size()) {
+        return true;
+      }
+    }
+    return false;
+  }
+  /*
+  preprocesses regions so that calculations can be speedier is subsequent calls
+  */
+  void process_regions(vector<vector<int>> &image,
+                       vector<vector<int>> &pixel_count) {
+    for (int i = 0; i < image.size(); i++) {
+      for (int j = 0; j < image[0].size(); j++) {
+        int up;
+        int left;
+        int diag;
+        if (valid(i, j - 1, image)) {
+          left = pixel_count[i][j - 1];
+        } else {
+          left = 0;
+        }
+        if (valid(i - 1, j, image)) {
+          up = pixel_count[i - 1][j];
+        } else {
+          up = 0;
+        }
+        if (valid(i - 1, j - 1, image)) {
+          diag = pixel_count[i - 1][j - 1];
+        } else {
+          diag = 0;
+        }
+        if (valid(i, j, image)) {
+          int total = image[i][j] + left + up - diag;
+          pixel_count[i][j] = total;
+        }
+      }
+    }
+  }
+  /*
+  creates a vector to store the pixel count in regions and matches the size to
+  image then processes the region. After processing a second vector sums is
+  created with the size of regions in the .dat file. The for loop iterates
+  through the regions calculating the total through the statement in the
+  if statement / else statement
+  */
+  vector<int> count_pixels_in_regions(
+      vector<vector<int>> &image,
+      vector<pair<pair<int, int>, pair<int, int>>> &regions) {
+    vector<vector<int>> pixel_count;
+    pixel_count.resize(image.size());
+    for (int i = 0; i < pixel_count.size(); ++i) {
+      pixel_count[i].resize(image[i].size());
+    }
+    process_regions(image, pixel_count);
+    vector<int> sums;
+    sums.resize(regions.size());
+    for (int i = 0; i < regions.size(); ++i) {
+      int total = 0;
+      int e, f, g, h;
+
+      e = regions[i].first.first;
+      f = regions[i].first.second;
+      g = regions[i].second.first;
+      h = regions[i].second.second;
+
+      if (e > 0) {
+        total = pixel_count[g][h] - pixel_count[g][f - 1] -
+                pixel_count[e - 1][h] + pixel_count[e - 1][f - 1];
+      } else {
+        total = pixel_count[g][h] - pixel_count[g][f - 1] - pixel_count[e][h] +
+                pixel_count[e][f - 1];
+      }
+
+      sums[i] = total;
+    }
+    return sums;
+  }
+};
---
+++ stest.cc
@@ -0,0 +1,167 @@
+//
+// File:Test_harness.cc
+// Purpose: Test harness for the class Count_Pixels for CS 4000 HW #2
+// Outline:
+//        1.)  Reads two file names from the command line --- one
+//          with the name of the pbm file, and one with the name of the box
+//          data//          file.
+//        2.) Opens the pbm file --> converts it to a vector of 0's and 1's.
+//        3.) Opens file containing region descriptions --> store it in a
+//        vector. 4.) Calls appropriate code from Count_Pixels to count the
+//        number of
+//            "on" pixels in a region.
+//        5.) Prints out the sum from each region.
+//
+// Author: David W. Juedes
+//
+// Note: In PBM files The bit '1' means black, '0' means white.
+//       We flip for this assignment.
+//
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <fstream>
+#include <iostream>
+#include <vector>
+using namespace std;
+// You write this part !!!
+#include "s.cc"
+//
+// Read pairs of points (e.g., (a,b) (c,d)).
+// Store them in the variable "boxes"
+//
+void read_boxes(istream &in,
+                vector<pair<pair<int, int>, pair<int, int>>> &boxes) {
+  while (!in.eof()) {
+    int a, b, c, d;
+
+    in >> a >> b >> c >> d;
+    if (!in.fail()) {
+      boxes.push_back(make_pair(make_pair(a, b), make_pair(c, d)));
+    }
+  }
+  // return boxes;
+}
+//
+// Read a PBM file -- store it in variable image.
+//
+void read_PBM(istream &in, vector<vector<int>> &image, int &n, int &m) {
+  string magic;
+  string t;
+  int xdim, ydim;
+
+  in >> magic;
+  getline(in, t);
+  while (in.peek() == '#') {
+    getline(in, t);
+  }
+  in >> xdim >> ydim;
+  // traverse the file data and store it in a 2 dimensional vector
+  // run through each column
+  string z;
+  getline(in, z);                // Consume newline.
+  for (int i = 0; i < ydim; i++) // Read per line.
+  {
+    vector<int> tmp;
+    // run though each row in terms of bytes
+    int limit = xdim / 8;
+    if ((xdim % 8) != 0)
+      limit++;
+    int p_count = 0;
+    for (int j = 0; j < limit; j++) {
+      // read byte[i][j]
+      unsigned char x;
+      x = in.get();
+      if (in.fail()) {
+        cout << "Error!!!!" << endl;
+      } else {
+        bitset<16> y(x);
+        for (int l = 7; l >= 0; l--) {
+          if (p_count < xdim) {
+            int clr;
+            if (y[l] == 1)
+              clr = 0;
+            else
+              clr = 1;
+            tmp.push_back(clr);
+            p_count++;
+          }
+        }
+      }
+    }
+    // push the row onto the matrix vector.
+    image.push_back(tmp);
+    if (tmp.size() != xdim) {
+      cout << i << " " << tmp.size() << endl;
+    }
+  }
+  n = ydim;
+  m = xdim;
+}
+
+int main(int argc, char *argv[]) {
+
+  ifstream fin;
+  fin.open(argv[1]);
+  // in case file fails to open for input, output error message
+  if (fin.fail()) {
+    cout << "Failed to open" << argv[1] << endl;
+    exit(-1);
+  }
+  ifstream fin1;
+  fin1.open(argv[2]);
+  // in case file fails to open for input, output error message
+  if (fin1.fail()) {
+    cout << "Failed to open" << argv[2] << endl;
+    exit(-1);
+  }
+
+  vector<vector<int>> image;
+  int n, m;
+  read_PBM(fin, image, n, m);
+
+  cerr << "Image Read" << n << "x" << m << endl;
+
+  vector<pair<pair<int, int>, pair<int, int>>> boxes;
+
+  read_boxes(fin1, boxes);
+
+  // Print out the bits.
+  // for (int i=0;i<n;i++) {
+  //  for (int j=0;j<50;j++) {
+  //    cout << image[i][j];
+  //  }
+  //  cout << endl;
+  //}
+
+  Count_Pixels obj;
+
+  vector<int> results;
+  // Time the code using Chrono
+  //
+  // Thanks to https://www.geeksforgeeks.org/chrono-in-c/
+  // for a hint about how to do this.
+  //
+  chrono::time_point<chrono::system_clock> start;
+
+  // Start the system clock.
+  start = chrono::system_clock::now();
+
+  results = obj.count_pixels_in_regions(image, boxes);
+
+  chrono::time_point<chrono::system_clock> end;
+
+  end = chrono::system_clock::now();
+
+  chrono::duration<double> total_time = end - start;
+
+  for (int i = 0; i < results.size(); i++) {
+    cout << "Pixels in region " << boxes[i].first.first << ","
+         << boxes[i].first.second << " --- " << boxes[i].second.first << ","
+         << boxes[i].second.second << endl;
+    cout << " = " << results[i] << endl;
+  }
+
+  cerr << "Base computation took " << total_time.count() << " seconds " << endl;
+  cerr << "Doesn't include reading/writing data" << endl;
+}
