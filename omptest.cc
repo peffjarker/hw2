d7a9879a87084a444d846474e4be219fa8464cac
Jeffery Parker <36011518+peffjarker@users.noreply.github.com>
2020-02-18 11:07:10 -0500

    Add files via upload

---
+++ omptest.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <omp.h>
+using namespace std;
+
+int main() {
+  omp_set_num_threads(2);
+  int a[1000] = {0};
+  for (int i = 0; i < 1000; ++i) {
+    a[i] = i;
+  }
+  int sum = 0;
+#pragma omp parallel for
+  for (int i = 0; i < 1000; ++i) {
+    if (a[i] < 10) {
+      sum++;
+    }
+  }
+
+  cout << sum;
+  return 0;
+}
